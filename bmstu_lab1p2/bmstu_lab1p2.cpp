4831f6495a8b0854e6b6f8d54eac14c044890087
AconCopper <ika.khazaradze@gmail.com>
2021-09-29 15:31:19 +0300

    Add project files.

---
+++ bmstu_lab1p2/bmstu_lab1p2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	double a, b, c, sqrtD, res1, res2;
+	printf_s("a="); scanf_s("%lf", &a);
+	printf_s("b="); scanf_s("%lf", &b);
+	printf_s("c="); scanf_s("%lf", &c);
+	sqrtD = sqrt(b * b - 4 * a * c);
+	res1 = (-b + sqrtD) / (2*a);
+	res2 = (-b - sqrtD) / (2*a);
+	std::cout << "x=" << res1 << " or " << res2;
+}
